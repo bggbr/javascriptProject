2f7d6e6142ac9e52d2cbd370e9a8b144bf9d16ab
bggbr <48645202+bggbr@users.noreply.github.com>
2020-10-05 14:38:26 +0900

    Add files via upload

--- browser101/WebAPIsProject1/main.js
+++ browser101/WebAPIsProject1/main.js
@@ -1,7 +1,26 @@
-document.addEventListener("mousemove", (event) => {
-  var cursorX = event.clientX;
-  var cursorY = event.clientY;
-  console.log(cursorX, ", ", cursorY);
-  const target = document.querySelector(".target");
-  target.innerHTML = `${cursorX}, ${cursorY}`;
+// document.addEventListener('mousemove', (event) => {
+//     var cursorX = event.clientX;
+//     var cursorY = event.clientY;
+//     console.log(cursorX, ', ', cursorY);
+//     const target = document.querySelector('.target');
+//     target.innerHTML = `${cursorX}, ${cursorY}`;
+//     console.log(event);
+// });
+
+const vertical = document.querySelector('.vertical');
+const horizontal = document.querySelector('.horizontal');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+document.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    vertical.style.left = `${x}px`;
+    horizontal.style.top = `${y}px`;
+    target.style.left = `${x}px`;
+    target.style.top = `${y}px`;
+    tag.style.left = `${x}px`;
+    tag.style.top = `${y}px`;
+    tag.innerHTML = `${x}px, ${y}px`;
 });
