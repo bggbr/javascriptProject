2fdd92da586c7b4948a9c6d62f0d0e0d0cf6e9a6
bggbr <jaejung603@gmail.com>
2021-01-09 00:16:27 +0900

    JS : Navbar menu click

--- portfolio/main.js
+++ portfolio/main.js
@@ -3,20 +3,28 @@
 // Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-// window.addEventListener('scroll', () => {
-// 	if (window.scrollY >= navbarHeight) {
-// 		navbar.style.position = 'fixed';
-// 		navbar.style.width = '100%';
-// 		navbar.style.backgroundColor = '#059449';
-// 	} else {
-// 		navbar.style.backgroundColor = 'transparent';
-// 	}
-// });
-
 window.addEventListener('scroll', () => {
 	if (window.scrollY >= navbarHeight) {
 		navbar.classList.add('navbar--dark');
 	} else {
 		navbar.classList.remove('navbar--dark');
 	}
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+// Check
+const menu = document.querySelector('#contact');
+console.log(menu);
+// Algorithm
+navbarMenu.addEventListener('click', (event) => {
+	const target = event.target;
+	const link = target.dataset.link;
+	if (link === null) {
+		return;
+	}
+	console.log(link);
+	const scrollTo = document.querySelector(link);
+	console.log(scrollTo); // null is not an object
+	scrollTo.scrollIntoView({ behavior: 'smooth', block: 'center' });
 });
